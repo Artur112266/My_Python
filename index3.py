86ff2a8334716c361c5171b91347a670c374f7b5
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 20:11:56 +0400

    about inport and math

---
+++ index3.py
@@ -0,0 +1,56 @@
+# where = input ("Where are you from ")
+# age = 31
+# print("Hallo, you are from " + where + " i am " + str(age))
+# p = 3.14
+# r = int(input("Please enter radius oy circle " ))
+# rountcircle = 2 * p * r**2 
+# # print(rountcircle) 
+# num = int(input("cankacac tiv "))
+# result = num ** 0.5
+# print(result) 
+
+# das 3
+# x = 100
+# y = 150
+# x = x + y 
+# print(x)
+# x = 100
+# x = x + 150
+# x  += 150
+# print(x)
+# z = 200
+# z -= 150
+# print(z)  
+
+
+# print(math.pi)
+# print(math.sqrt(16))
+# from math import *
+# r = int(input("enter r = "))
+# s = 2 * pi * r**2
+# print(sqrt(25))
+# print(s)
+# print(e)
+# print(sin(45))
+
+# import random
+# # print(random.random)
+# # print(random.randint(1,6))
+# x = random.randint(1,6)
+# y = random.randint(1,6)
+# c = x + y
+# while c > 5:
+	
+# 	print(c)
+# 	break
+
+# import random as r
+# print(r.randrange(0,100,8))
+
+# import time
+# x = int(input("x = "))
+# y = int(input("y = "))
+# time.sleep(2)
+# print('x * y = ' + str(x * y))
+
+
