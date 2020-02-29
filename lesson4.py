5956088b15f191ac7537a9db325a513fbf2483a9
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 20:15:24 +0400

    int, impord random

---
+++ lesson4.py
@@ -0,0 +1,64 @@
+# x = int(input("sum = "))
+# print(5 > x)
+# print(8 < x)
+# print(4 == x)
+# print(9 >= x)
+# print(1 <= x)
+# print(11 != x)
+
+# import random
+# name = input("What is your name? ")
+# x = int(input("enter num "))
+# y = random.randint(1,100)
+# z = int(input("enter num "))
+# p = random.randint(1,100)
+# resultname = x + z
+# # resultuser = y + p
+# # print(name, resultname, resultname > resultuser, "user = ", resultuser)
+
+# name = input("What is your name miss? ")
+# age = int(input("How old are you ?"))
+# # print(age > 18 and age < 27 )
+
+# name = input("What is your name miss? ")
+# age = int(input("How old are you ?"))
+# print(age > 18 or age == 27 )
+
+# name = input("What is your name miss? ")
+# age = int(input("How old are you ?"))
+# print(not(age > 18 and age < 27))
+
+# name = "Artur"
+# print(id(name))
+# age = 19
+# print(id(age))
+
+# name = "Artak"
+# name2 = "Artur"
+# print(name is name2)
+
+# name = input("What is your name miss? ")
+# name2 = 'anna'
+# print(id(name))
+# print(id(name2))
+# print(name == "Anna" or name == "anna")
+
+# sar = int(input('vorqan e amenabardzr leran bardzrutyun@? '))
+# print(sar == 8848 or  sar == 8884)
+
+# name = 'Ani'
+# name2 = 'anna'
+# print("a" in name and "a" not in name2)
+
+# name = input("What is your name? ")
+# print ("a" in name)
+
+# dasaran = "artak gevor gohar narine ani gexam saqo luso gayan anna aram ashot valod"
+# print ("Ani" in dasaran or "ani" not in dasaran)
+
+# xumb = "art artak aram anna syuzanna meri nara gago bella vlad geras vard"
+# print ("art" in xumb and "Artash" not in xumb or "gago" in xumb)
+
+tnayin useric heraxosa enq uzum guyn enq uzum
+harcnum enq ardyoq sarnaranum ka banan xndzor kam narinj, ete luys ka tox mna ete chka tox hani
+ete mirg chka gna ar
