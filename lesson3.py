83ae57932ffde1bf815f28dd72b7c1ac108e22ad
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 20:14:08 +0400

    math and inport

---
+++ lesson3.py
@@ -0,0 +1,79 @@
+# where = input ("Where are you from ")
+# age = 31
+# print("Hallo, you are from " + where + " i am " + str(age))
+# p = 3.14
+# r = int(input("Please enter radius oy circle " ))
+# rountcircle = 2 * p * r**2 
+# # print(rountcircle) 
+# num = int(input("cankacac tiv "))
+# result = num ** 0.5
+# print(result) 
+
+# das 3
+# x = 100
+# y = 150
+# x = x + y 
+# print(x)
+# x = 100
+# x = x + 150
+# x  += 150
+# print(x)
+# z = 200
+# z -= 150
+# print(z)  
+
+
+# print(math.pi)
+# print(math.sqrt(16))
+# from math import *
+# r = int(input("enter r = "))
+# s = 2 * pi * r**2
+# print(sqrt(25))
+# print(s)
+# print(e)
+# print(sin(45))
+
+# import random
+# # # print(random.random)
+# # # print(random.randint(1,6))
+# while True:
+
+# 	x = random.randint(1,6)
+# 	y = random.randint(1,6)
+
+# 	c = x + y
+# 	if c > 11:
+# 		print(x)
+# 		print(y)
+# 		print(c)
+# 		break
+
+# import random as r
+# print(r.randrange(0,100,8))
+
+# import time
+# x = int(input("x = "))
+# y = int(input("y = "))
+# time.sleep(2)
+# print('x * y = ' + str(x * y))
+
+# import os
+# print(os.getcwd())
+
+# import calendar
+# y = int(input("input the year : "))
+# m = int(input("input the month : "))
+# print(calendar.month(y,m))
+
+# import datetime
+# # # x = datetime.datetime.now ()
+# # # print (x)
+# # # print (datetime.date.today())
+
+# born = int(input('when you was born'))
+# x = datetime.datetime.now()
+# result = x.year - born
+# print('you are ' + str(result) + ' years old')
+
+www.stackoverflow.com
+ 
