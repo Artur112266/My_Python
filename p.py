6d26818c6a08dc2726fdb62ed4932d70c3df1972
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 20:05:36 +0400

    my second commit

---
+++ p.py
@@ -0,0 +1,29 @@
+print("Hello World")
+x=5
+y=10
+print(x+y)
+name = "Artur"
+surname = "Avetisyan"
+print(name,surname)
+z=9
+n=9.9
+q=z*n
+# print(q)
+name="Artur"
+surname="Avetisyan"
+x=2020
+# y=1989
+# z=x-y
+# print("I am", name, surname, "i",z,"years old" )
+name=input("Hi, what is your name")
+year=input("Hello, how old are you")
+print("Hi",name, year)
+nationality=input("Hi, what is your nationality")
+where=input("whera are you living")
+year=input("how old are you")
+world=input("what is your favorit sport")
+ov=input("who is teach you this lesson")
+print("I am from", nationality,",", "i live in",",", where, "i am",",", year, "my faveorit sport is",",", world, "me teach is",",",ov,":",)
+
+
+
