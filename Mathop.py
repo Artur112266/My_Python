06d936e038c33d7c44e71ee3010522914472d625
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 19:49:31 +0400

    my first commit

---
+++ Mathop.py
@@ -0,0 +1,17 @@
+# x = int(input ( "input x = " ))
+# y = int(input ( "input y = " ))
+# add = x + y
+# sub = x - y
+# mul = x * y
+# div1 = x / y
+# div2 = x // y
+# mod = x % y
+# squ = x ** y
+# print("x + y =",add)
+# print("x - y =",sub)
+# print("x * y =",mul)
+# print("x / y =",div1)
+# print('x // y =',div2)
+# print('x % y =',mod)
+# print("x ** y =",squ)
+
