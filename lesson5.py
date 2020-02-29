d9c895cfd2f6430665b4fe6fb5ee29c824e9f3bb
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 20:17:40 +0400

    Logical operators

---
+++ lesson5.py
@@ -0,0 +1,44 @@
+# print("please input (8) for right othervise no" )
+
+# item_banana = input("Have you banana in refrigiretor ? ") == "8"
+# print(item_banana)
+
+# print("Please input (yes) for rigth othervise (no)")
+# item_banana = input("Have you banana in refrigiretor ? ") == "yes"
+# item_tomato = input("Have you tomato in refrigirator ? ") == "yes"
+# item_aplle = input("Have you apple in refrigirator ? ") == "yes"
+# item_kiwi = input("Have you kiwi in refrigirator ? ") == "yes"
+# item_mango = input('Have you mango in refrigirator ? ') == "yes"
+# home_electrocity = input("have you electrocity at home ? ") == 'yes'
+# remov_banana = item_banana and not home_electrocity
+# remove_tomato = item_tomato and not home_electrocity
+# remove_apple = item_aplle and not home_electrocity
+# remove_kiwi = item_kiwi and not home_electrocity
+# remove_mango = item_mango and not home_electrocity
+# print("hanel banana sarnaranic: " + str(remov_banana))
+# print("hanel tomato sarnaranic: " + str(remove_tomato))
+# print("hanel apple sarnaranic: " + str(remove_apple))
+# print("hanel kiwi sarnaranic: " + str(remove_kiwi))
+# print("hanel mango sarnaranic: " + str(remove_mango))
+
+# print("Go bring banana: " + str(not item_banana))
+# print("Go bring tomato: " + str(not item_tomato))
+# print("Go bring apple: " + str(not item_aplle))
+# print("Go bring kiwi: " + str(not item_kiwi))
+# print("Go bring mango: " + str(not item_mango))
+
+
+
+# print("Please input (y) for right othervise (n)")
+
+# item_iphone = input("Have you iphone ? ") == "y"
+# print("Have you iphone:" + str(item_iphone))
+# color_iphone = input("What color your iphone ? ") == 'red'
+# no_iphone = item_iphone and color_iphone
+# print("Your iphone is red " + str(no_iphone))
+# iphone_7 = input("What is your iphone model ? ") == "7"
+# no_iphone_7 = item_iphone and iphone_7
+# print("You have iphone 7 " + str(no_iphone_7))
+# energy_iphone = input("Have you energy iphone ? ") == "y"
+# print("It is good that you have energy iphone " + str(energy_iphone))
+
