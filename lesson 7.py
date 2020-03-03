5916d61acc9aa129e1fab067a3ef50c399abab6d
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-03-03 21:17:03 +0400

    lesson7, if, elif and else

--- lesson 7.py
+++ lesson 7.py
@@ -1,1 +1,71 @@
-if else.py
+# import sys
+# x = [1, 2, 2, 3, 3, 6]
+# print(sys.getsizeof(x))
+
+# a = 8
+# b = 7
+# if a > b:
+# 	print("a is big")
+# elif b > a:
+# 	print("b is big")
+# else:
+# 	print("b = a")
+
+# big_country = input("Which is tne bigest country in world: ")
+# if big_country == "china" or big_country == "China" or big_country == "chinastan" or big_country == "Chinastan":
+# 	print("maladec")
+# else:
+# 	print("false")
+
+# import random
+# a = random.randint(1, 6)
+# b = random.randint(1, 6)
+# c = random.randint(1, 6)
+# d = random.randint(1, 6)
+# result = (a + b)
+# print ("computer = ", a, "+", b, "+", result)
+# result1 = (c + d)
+# print("player = ", c, "+", d, "=", result1)
+
+# if result > result1:
+# 	print("win computer")
+# elif result < result1:
+# 	print("win player")
+# else:
+# 	print("equal")
+
+# year = int(input("enter year "))
+# if year % 400 == 0 and year % 100 == 0:
+# 	print("nahanj tari chi")
+
+
+	
+# 		print("nahanj tari")
+# else:
+# 	if year % 4 == 0 and year % 400 != 0:
+
+# year = int(input("enter year "))
+# if year % 4 != 0 or year % 100 == 0 and year % 400 != 0:
+# 	print("nahanj tari chi")
+# else:
+# 	print("nahanj tari e") 
+
+
+# year = int(input("enter year "))
+# if year % 4 == 0 or year % 100 == 0 and year % 400 != 0:
+# 	print("nahanj tari chi")
+# else:
+# 	print("nahanj tari e") 
+
+
+# 	useric tariq enq uzum, u asum enq 70 meca te che,
+import random
+age = random.randint(1, 90)
+
+if age > 25 and age < 35:
+	print(age, "tarekan eq, Duq karox eq HH varchapet darnal")
+elif age < 18:
+	print(age, "tarekan eq, Duq anchapahas eq yev cheq karox")
+else:
+	print(age,"tarekan eq, Duq cheq karox @ntrvel HH Varchapet")
+
