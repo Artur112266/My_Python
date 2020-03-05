f5025175477b62b30048e7984756d4e89d5dc87b
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-03-05 21:11:34 +0400

    'xndir'

---
+++ lesson8.py
@@ -0,0 +1,76 @@
+# name = "aram artur vardan karen gago"
+# i = input()
+# if i in name:
+# 	print("yes")
+# else:
+# 	print("no")
+
+
+
+
+
+
+
+
+
+# shan tariq@
+# age = int(input("enter human age "))
+# if age <= 2:
+# 	i = age * 10.5
+	
+# 	print(i)
+# else:
+# 	y = (age-2)*4+21
+# 	print(y)
+
+
+
+
+
+
+
+# celciuc convert farenheit
+
+# celcius = int(input("enter celcius "))
+# if celcius == 0:
+# 	print('farenheit = 32')
+# else:
+# 	print(32 + 1.8 * celcius)
+
+
+
+
+
+
+
+# farenheit convert celciuc
+
+# farenheit = float(input("farenheit ="))
+# if farenheit == 32:
+# 	print('celcius = 0')
+# else:
+# 	i = farenheit-32
+# 	y = i / 1.8
+# 	print(round(y)) 
+
+
+
+
+
+# mail = input("please enter your mail")
+# i = "@"
+# if i in mail:
+# 	print("yes")
+# else:
+# 	print("no")
+
+
+
+
+
+tnayin 
+year  
+name 
+surname
+mail
+password
