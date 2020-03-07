7d09bddfbfe21069ce39a5e7577ba10c853f4ad3
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-03-07 20:58:44 +0400

    Loop, While, For, Break, Continue

---
+++ lesson9.py
@@ -0,0 +1,65 @@
+# Loop
+# names = "1223456"
+# for i in names:
+# 	print(i)
+# print(names[4])
+
+# range
+# for i in range(8):
+# 	print("Ani")
+
+# x = 0
+# for i in range(5):
+# 	x += 1
+# 	print(x)
+
+
+# x = 0
+# for i in range(7):
+# 	x += 1
+# 	if x != 7:
+# 		print("right")
+# 	else:
+# 		print("error")
+
+# for x in range(0, 12, 2): 
+# 	print(x)
+
+# name = "123456789"
+# surname = "Avetisyan"
+# for x in name:
+# 	for y in surname:
+# 		print(x,y)
+# i = 1
+# while i <= 6:
+# 	print (i)
+# 	i += 1
+
+# i = 1
+# while i < 6:
+# 	print(i)
+# 	if i == 2:
+# 		break
+# 	i += 1
+
+
+# zar Nardi
+# import random
+# comp = random.randrange(1,7)
+# comp1 = random.randrange(1,7)
+# COMP = comp + comp1
+# while True:
+# 	user = random.randrange(1,7)
+# 	user1 = random.randrange(1,7)
+# 	USER = user + user1
+# 	if USER > COMP:
+# 		print(USER, ">", COMP)
+# 		break
+# 	elif USER == 12 and COMP == 12:
+# 		print(USER, "=", COMP)
+# 		break
+name = "Arsen"
+for i in name:
+	if i == "r":
+		continue
+	print(i)
