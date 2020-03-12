5faf36a316d05c90bd2a384eb7963d41bb9d8c22
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-03-12 21:02:11 +0400

    list

---
+++ lesson11.py
@@ -0,0 +1,77 @@
+# list
+
+# fruits = ['banana', 'apple', 'cherry']
+# print(fruits)
+
+# numbers = [34,56,-456, 7.56, -2.34]
+# print(numbers[1])
+
+# clas = ["Artur", "Aram", "Karen", "David", "Seroj", "Anna", "Ani", "Armine", "Artyom"]
+
+# for i in clas:
+# 	if "Artyom" not in clas:
+# 		print("Artyom is not in our class")
+# 		break
+# 	else:
+# 		print("Artyom")
+# 		break
+
+# negative indexing
+
+# numbers = [34, 56, -456, 7.56, -2.34]
+# print(numbers[-1])
+
+# fruits = ['banana', 'apple', 'cherry', 'kiwi']
+# print(fruits[1:3])
+# print(fruits[:3])
+# print(fruits[1:])
+# print(fruits[-3:-1])
+
+# Change item Value
+# fruits = ["banana", "apple", 'cherry']
+# fruits[1] = ""
+# print(fruits)
+
+# len()
+# fruits = ["banana", "apple", 'cherry', 'HHHH']
+# print(len(fruits))
+
+# insert()
+# fruits = ["banana", "apple", 'cherry']
+# fruits.insert(2, "orange")
+# print(fruits)
+
+# remove()
+# fruits = ["banana", "apple", 'cherry']
+# fruits.remove("apple")
+# print(fruits)
+
+# pop()
+# fruits = ["banana", "apple", 'cherry']
+# fruits.pop()
+# print(fruits)
+
+# del
+# fruits = ["banana", "apple", 'cherry']
+# del fruits[0]
+# del fruits
+# print(fruits)
+
+
+# extend()
+# fruits = ["banana", "apple", 'cherry']
+# numbers = [34, 56, -456, 7.56, -2.34]
+# fruits.extend(numbers)
+# print(fruits)
+
+# sort()
+# numbers = [34, 56, -456, 7.56, -2.34]
+# numbers.sort()
+# print(numbers[0])
+
+# reverse()
+# numbers = [34, 56, -456, 7.56, -2.34, 157, 2, -8]
+# numbers.sort()
+# print(numbers)
+# print(numbers[-3:])
+# print(numbers[:3])
